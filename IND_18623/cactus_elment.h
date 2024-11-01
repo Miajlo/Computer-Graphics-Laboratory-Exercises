41c967d56136392a79e1c8d53df359040a62868a
Miajlo <mihajlod.mes@gmail.com>
2024-11-01 22:28:05 +0100

    Added enum for meta file type. Added scale for every element.

--- IND_18623/cactus_elment.h
+++ IND_18623/cactus_elment.h
@@ -1,9 +1,16 @@
 #pragma once
 #include <atltypes.h>
 
+enum META_TYPE {
+	GREEN_PART,
+	YELLOW_PART
+};
+
 
 struct cactus_element {
 	CPoint position;
+	int sx; //scale
+	int sy;
 	float angle;
-
+	META_TYPE type;
 };
