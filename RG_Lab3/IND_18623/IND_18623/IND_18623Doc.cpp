f5632490849ddbaa70ac424f0803d6a0d6bd235e
Miajlo <mihajlod.mes@gmail.com>
2024-11-14 16:12:51 +0100

    Created new project for 3rd lab in comuper graphics, in gdi

---
+++ RG_Lab3/IND_18623/IND_18623/IND_18623Doc.cpp
@@ -0,0 +1,138 @@
+
+// IND_18623Doc.cpp : implementation of the CIND18623Doc class
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS can be defined in an ATL project implementing preview, thumbnail
+// and search filter handlers and allows sharing of document code with that project.
+#ifndef SHARED_HANDLERS
+#include "IND_18623.h"
+#endif
+
+#include "IND_18623Doc.h"
+
+#include <propkey.h>
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CIND18623Doc
+
+IMPLEMENT_DYNCREATE(CIND18623Doc, CDocument)
+
+BEGIN_MESSAGE_MAP(CIND18623Doc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CIND18623Doc construction/destruction
+
+CIND18623Doc::CIND18623Doc() noexcept
+{
+	// TODO: add one-time construction code here
+
+}
+
+CIND18623Doc::~CIND18623Doc()
+{
+}
+
+BOOL CIND18623Doc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: add reinitialization code here
+	// (SDI documents will reuse this document)
+
+	return TRUE;
+}
+
+
+
+
+// CIND18623Doc serialization
+
+void CIND18623Doc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: add storing code here
+	}
+	else
+	{
+		// TODO: add loading code here
+	}
+}
+
+#ifdef SHARED_HANDLERS
+
+// Support for thumbnails
+void CIND18623Doc::OnDrawThumbnail(CDC& dc, LPRECT lprcBounds)
+{
+	// Modify this code to draw the document's data
+	dc.FillSolidRect(lprcBounds, RGB(255, 255, 255));
+
+	CString strText = _T("TODO: implement thumbnail drawing here");
+	LOGFONT lf;
+
+	CFont* pDefaultGUIFont = CFont::FromHandle((HFONT) GetStockObject(DEFAULT_GUI_FONT));
+	pDefaultGUIFont->GetLogFont(&lf);
+	lf.lfHeight = 36;
+
+	CFont fontDraw;
+	fontDraw.CreateFontIndirect(&lf);
+
+	CFont* pOldFont = dc.SelectObject(&fontDraw);
+	dc.DrawText(strText, lprcBounds, DT_CENTER | DT_WORDBREAK);
+	dc.SelectObject(pOldFont);
+}
+
+// Support for Search Handlers
+void CIND18623Doc::InitializeSearchContent()
+{
+	CString strSearchContent;
+	// Set search contents from document's data.
+	// The content parts should be separated by ";"
+
+	// For example:  strSearchContent = _T("point;rectangle;circle;ole object;");
+	SetSearchContent(strSearchContent);
+}
+
+void CIND18623Doc::SetSearchContent(const CString& value)
+{
+	if (value.IsEmpty())
+	{
+		RemoveChunk(PKEY_Search_Contents.fmtid, PKEY_Search_Contents.pid);
+	}
+	else
+	{
+		CMFCFilterChunkValueImpl *pChunk = nullptr;
+		ATLTRY(pChunk = new CMFCFilterChunkValueImpl);
+		if (pChunk != nullptr)
+		{
+			pChunk->SetTextValue(PKEY_Search_Contents, value, CHUNK_TEXT);
+			SetChunkValue(pChunk);
+		}
+	}
+}
+
+#endif // SHARED_HANDLERS
+
+// CIND18623Doc diagnostics
+
+#ifdef _DEBUG
+void CIND18623Doc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CIND18623Doc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CIND18623Doc commands
