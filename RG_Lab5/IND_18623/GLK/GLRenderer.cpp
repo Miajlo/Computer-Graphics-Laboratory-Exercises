2579949777abf29d7ecba944a5d853731dddb83d
Miajlo <mihajlod.mes@gmail.com>
2025-06-06 19:35:44 +0200

    We are going to mars.

--- RG_Lab5/IND_18623/GLK/GLRenderer.cpp
+++ RG_Lab5/IND_18623/GLK/GLRenderer.cpp
@@ -564,11 +564,14 @@ void CGLRenderer::SetupLigting()
     glEnable(GL_LIGHTING);
     glEnable(GL_LIGHT0);
 
-    GLfloat lightDir[] = { 1.0f, 1.0f, 0.0f, 0.0f };  // directional light
-    glLightfv(GL_LIGHT0, GL_POSITION, lightDir);
+    GLfloat diffuse[] = { 1.0f, 0.0f, 0.0f, 1.0f };
+    GLfloat ambient[] = { 0.2f, 0.0f, 0.0f, 1.0f };
+    GLfloat specular[] = { 1.0f, 0.0f, 0.0f, 1.0f };
 
-    GLfloat diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
     glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+
 
     GLfloat position[] = { 10.0f, 10.0f, 10.0f, 1.0f };  // positional light
     glLightfv(GL_LIGHT0, GL_POSITION, position);
